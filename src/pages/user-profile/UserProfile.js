413773e179d43ba9f81a4a820c3445f82a660c3a
veronika <veronika212.vyhlidalova@gmail.com>
2018-06-26 11:21:29 +0200

    acc css-modules and css reset to project

--- src/pages/user-profile/UserProfile.js
+++ src/pages/user-profile/UserProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { getUserProfile } from '../../sagas/userProfileSaga';
+import styles from './user-profile.css';
 
 class UserProfile extends Component {
   componentDidMount() {
@@ -9,7 +10,12 @@ class UserProfile extends Component {
   }
 
   render() {
-    return <div>User Profile</div>;
+    console.log(styles, 'styles');
+    return (
+      <div className={styles.userProfile}>
+        <p>User Profile</p>
+      </div>
+    );
   }
 }
 
